37c65f9dc472b45b13d3a79bc4f3404b546a80a4
shruti <shrutigupta5555@gmail.com>
2021-11-07 13:58:06 +0530

    twilio

--- app.py
+++ app.py
@@ -1,10 +1,32 @@
 from flask import Flask, render_template, request
+import os
+from twilio.rest import Client
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
+account_sid = os.getenv('ACC_SID')
+auth_token = os.getenv("AUTH_TOKEN")
+client = Client(account_sid, auth_token)
+
+def send_msg():
+    message = client.messages \
+                .create(
+                     body="Join Earth's mightiest heroes. Like Kevin Bacon.",
+                     from_='+14844168079',
+                     to='+918928581039'
+                 )
+    print("success============")
+    # print(message.sid)
+
 @app.route('/')
 def index(): 
+    # send_msg()
     return render_template('index.html')
 
 @app.route('/level/1')
