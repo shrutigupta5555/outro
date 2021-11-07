5570dcb45ac0b85104358aa138caa314faf3ca61
shruti <shrutigupta5555@gmail.com>
2021-11-07 12:50:51 +0530

    add ui

--- app.py
+++ app.py
@@ -8,8 +8,16 @@ def index():
     return render_template('index.html')
 
 @app.route('/level/1')
-def level(): 
-    return render_template('game.html')
+def level1(): 
+    return render_template('level1.html')
+
+@app.route('/success')
+def success(): 
+    return render_template('success.html')
+
+@app.route('/fail')
+def fail(): 
+    return render_template('fail.html')
 
 
 if __name__ == "__main__":
